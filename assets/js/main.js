5e933800a615812984c19f8e7499283bdb9af9c1
Pasquale Aragosa <aragosap@gmail.com>
2022-10-10 15:59:57 +0200

    add bonus 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,23 +8,25 @@ for (let i = 1; i <= 100; i++) {
         let liEl = document.createElement('li');
         liEl.append(`Fizz`);
         ulEL.append(liEl);
+        liEl.style.backgroundColor = "red";
         console.log("Fizz");
     } else if ((i%5 == 0) && !(i%3 == 0)) {
         let liEl = document.createElement('li');
         liEl.append(`Buzz`);
         ulEL.append(liEl);
+        liEl.style.backgroundColor = "lightblue";
         console.log("Buzz");
     } else if ((i%3 == 0) && (i%5 == 0)) {
         let liEl = document.createElement('li');
         liEl.append(`FizzBuzz`);
         ulEL.append(liEl);
+        liEl.style.backgroundColor = "orange";
         console.log("FizzBuzz");
     } else {
         let liEl = document.createElement('li');
         liEl.append(`${i}`);
+        liEl.style.backgroundColor = "lightgreen";
         ulEL.append(liEl);
     }
-
-    
 
 }
