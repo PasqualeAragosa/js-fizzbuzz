03f071b5eb0c06593bb7f724feafa336e345764f
Pasquale Aragosa <aragosap@gmail.com>
2022-10-10 16:43:47 +0200

    change if conditions order

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,24 +5,24 @@ wrapperEl.append(ulEL);
 
 
 for (let i = 1; i <= 100; i++) {
-    if((i%3 == 0) && !(i%5 == 0)) {
+    if((i % 3 == 0) && (i % 5 == 0)) {
         let liEl = document.createElement('li');
-        liEl.append(`Fizz`);
+        liEl.append(`FizzBuzz`);
         ulEL.append(liEl);
         liEl.style.backgroundColor = "red";
-        console.log("Fizz");
-    } else if ((i%5 == 0) && !(i%3 == 0)) {
+        //console.log("FizzBuzz");
+    } else if (i % 5 == 0) {
         let liEl = document.createElement('li');
         liEl.append(`Buzz`);
         ulEL.append(liEl);
         liEl.style.backgroundColor = "lightblue";
-        console.log("Buzz");
-    } else if ((i%3 == 0) && (i%5 == 0)) {
+        //console.log("Buzz");
+    } else if (i % 3 == 0) {
         let liEl = document.createElement('li');
-        liEl.append(`FizzBuzz`);
+        liEl.append(`Fizz`);
         ulEL.append(liEl);
         liEl.style.backgroundColor = "orange";
-        console.log("FizzBuzz");
+        //console.log("Fizz");
     } else {
         let liEl = document.createElement('li');
         liEl.append(`${i}`);
